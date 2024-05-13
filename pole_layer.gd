5f4c1b9128d1ffc6513a92e387181f3564c87df8
Lenetis <70149832+Lenetis@users.noreply.github.com>
2024-05-13 20:41:50 +0200

    Pole layer super ability fix

--- pole_layer.gd
+++ pole_layer.gd
@@ -6,7 +6,6 @@ var held_pole
 var pole_base
 
 var super_counter = 0
-var super_bool = false
 
 @rpc("any_peer", "call_local", "reliable")
 func shoot(dir):
@@ -15,17 +14,15 @@ func shoot(dir):
 	else:
 		super_counter = 0
 		
-	if super_counter < 4:
-		pole(dir)
+	pole(dir)
 		
 	if super_counter >= 4:
+		print("POLE LAYER!!!!")
 		super_counter = 0
-		for pole in self.all_poles:
-			pole.freeze = super_bool
+		for pole in self.all_poles.slice(0, -3):
+			pole.freeze = false
 			pole.gravity_scale = 1
 			pole.mass = 2
-			
-		super_bool = not super_bool
 
 @rpc("any_peer", "call_local", "reliable")
 func pole(dir):
